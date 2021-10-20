1121d95bec2dc00021bb44eb7b747899b99a0575
Derrick Otte <derrickotte@gmail.com>
2021-10-19 20:30:54 -0400

    Add Nav component
    - Added a basic nav component to App.vue

---
+++ src/components/Nav.vue
@@ -0,0 +1,14 @@
+<template>
+  <nav>
+    <router-link to="/">Home</router-link>
+    <router-link to="/">Research</router-link>
+    <router-link to="/">Publications</router-link>
+    <router-link to="/">People</router-link>
+    <router-link to="/about">Contact</router-link>
+  </nav>
+</template>
+<script>
+  export default {
+    name: 'Nav'
+  }
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,6 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-  </div>
+  <section class="home">
+  </section>
 </template>
 
 <script>
