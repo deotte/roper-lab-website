1c58cadc62f655789e60268f2dce219d1f588639
Derrick Otte <derrickotte@gmail.com>
2021-10-20 20:58:10 -0400

    Add initial nav
    - Finished the initial layout for the nav component
    - Have a temporary spot for the logo (future need)
    - Added some reset styles for a tags
    - Added a padding utility class

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,10 +1,15 @@
 <template>
-  <nav class="flex-row">
-    <router-link to="/">Home</router-link>
-    <router-link to="/">Research</router-link>
-    <router-link to="/">Publications</router-link>
-    <router-link to="/">People</router-link>
-    <router-link to="/about">Contact</router-link>
+  <nav class="flex-row padding">
+    <div class="logo flex-row">
+      <h1>Roper Lab</h1>
+    </div>
+    <div class="links flex-row">
+      <router-link to="/">Home</router-link>
+      <router-link to="/">Research</router-link>
+      <router-link to="/">Publications</router-link>
+      <router-link to="/">People</router-link>
+      <router-link to="/about">Contact</router-link>
+    </div>
   </nav>
 </template>
 <script>
