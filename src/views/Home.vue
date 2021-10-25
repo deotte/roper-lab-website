222fbf73aa6c92031a7c169f08d23d9fe08bc67e
Derrick Otte <derrickotte@gmail.com>
2021-10-25 15:56:05 -0400

    Add initial welcome page
    - Added some initial markup for the homepage "welcome page"
    - Added a lot of new css
    - Still left to do: add utility classes and make them work

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,35 @@
 <template>
-  <section class="home">
+  <section class="home padding flex-column center">
+    <div class="header">
+      <h1>Our Research</h1>
+    </div>
+    <div class="content">
+      <div class="text flex-column center">
+        <p>
+          Maecenas interdum augue vitae convallis pharetra. Fusce in nisi eu arcu semper dictum id vel risus. 
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc quis eros eleifend, tincidunt leo non, blandit ante. Phasellus at euismod velit. 
+          Duis bibendum nunc ut est tristique laoreet. Praesent malesuada venenatis nibh, et dapibus tellus ornare quis. Vivamus vulputate pretium blandit. 
+          Mauris lectus nibh, mattis pharetra turpis eu, varius mollis velit. 
+          Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
+        </p>
+        <p class="margin-top">
+          Ut sodales orci vel porta laoreet. Duis malesuada imperdiet ex in ultricies. 
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus molestie ultrices tellus. 
+          Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Suspendisse tincidunt accumsan condimentum. 
+          Aenean eu congue risus.
+        </p>
+        <router-link to="/" class="action-button extra-margin-top">
+          View our research
+        </router-link>
+      </div>
+      <div class="picture flex-column center l-padding">
+        <div class="placeholder"></div>
+        <span class="margin-top text-center">
+          Dolor sit amet, consectetur adipiscing elit. Nunc quis eros eleifend, tincidunt leo non, blandit ante. 
+          Phasellus at euismod velit.
+        </span>
+      </div>
+    </div>
   </section>
 </template>
 
