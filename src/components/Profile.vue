07af09e6467977d09eda89da9a168329f2ce5f3c
Derrick Otte <derrickotte@gmail.com>
2021-10-26 14:55:38 -0400

    Finish initial /people interface
    - Added profile component
    - Tried to reuse content div to make interface consistent
    - Used a people.json for people records to iterate over

---
+++ src/components/Profile.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="profile flex-row center">
+    <div class="picture flex-column center"><div class="placeholder"></div></div>
+    <div class="text flex-column center lr-padding">
+      <h3>{{ name }}</h3>
+      <template v-if="Array.isArray(title)">
+        <span v-for="(title, index) of title"
+              :key="index"
+              class="margin-top">
+          {{ title }}
+        </span>
+      </template>
+      <template v-else>
+        <span class="margin-top">{{ title }}</span>
+      </template>
+      <p class="margin-top">{{ description }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Profile',
+    props: {
+      name: { type: String, required: true },
+      title: { type: [String, Array], required: true },
+      picture: { type: String, required: false },
+      description: { type: String, required: true }
+    }
+  }
+</script>
---
+++ src/views/People.vue
@@ -0,0 +1,31 @@
+<template>
+  <section class="people padding flex-column center">
+    <div class="content flex-row center">
+      <template v-if="people.length > 0">
+        <Profile v-for="(person, index) of people"
+                :title="person.title"
+                :name="person.name"
+                :description="person.description"
+                :key="index"></Profile>
+      </template>
+    </div>
+  </section>
+</template>
+
+<script>
+  import people from "@/assets/people.json"
+  import Profile from "@/components/Profile.vue"
+
+  export default {
+    name: 'People',
+    components: { Profile },
+    data() {
+      return {
+        people: []
+      }
+    },
+    created() {
+      this.people = people;
+    }
+  }
+</script>
