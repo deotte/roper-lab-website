34a0e8c7ff3930abc6fdd0193d2e23c43176cc00
Derrick Otte <derrickotte@gmail.com>
2022-01-17 13:38:15 -0500

    Add bootstrap to Contact Us page

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -1,8 +1,13 @@
 <template>
   <section class="contact padding flex-row center">
-    <div class="content flex-row margin-top" v-if="loaded">
-      <div class="text flex-column center r-padding">
-        <div class="pictures">
+    <div class="container mt-2">
+      <div class="row mt-lg-0">
+        <div class="col">
+          <h2>Connect with Us</h2>
+        </div>
+      </div>
+      <div class="row mt-lg-4 mt-xl-2">
+        <div class="col lg-8 d-none d-lg-block me-lg-4">
           <a 
             class="twitter-timeline"
             href="https://twitter.com/RoperLab_IUPUI?ref_src=twsrc%5Etfw"
@@ -10,21 +15,24 @@
             Tweets by RoperLab_IUPUI
           </a>
         </div>
-      </div>
-      <div class="find-us flex-column center l-padding" v-if="locations.length > 0">
-        <h2>Find Us</h2>
-        <template v-for="(location, index) of locations" :key="index">
-          <h4 class="extra-margin-top">{{ location.title }}</h4>
-          <template v-for="(field, index) of location.fields" :key="index">
-            <a class="margin-top" :href="field"
-               v-if="field.includes('https:')">
-              {{ field }}
-            </a>
-            <span class="margin-top" v-else>
-              {{ field }}
-            </span>
+        <div class="col-lg-4 mt-2 mt-md-4 ms-lg-4">
+          <template v-for="(location, index) of locations" :key="index">
+            <h4 :class="marginClass(index)">
+              {{ location.title }}
+            </h4>
+            <div class="fields mt-4 ms-2">
+              <template v-for="(field, index) of location.fields" :key="index">
+                  <a :href="field"
+                     v-if="field.includes('https:')">
+                    {{ field }}
+                  </a>
+                  <span class="mt-3" v-else>
+                    {{ field }}
+                  </span>
+              </template>
+            </div>
           </template>
-        </template>
+        </div>
       </div>
     </div>
   </section>
@@ -50,6 +58,14 @@
       twitterScript.setAttribute('charset', 'utf-8');
       document.head.appendChild(twitterScript);
       this.loaded = true;
+    },
+    methods: {
+      isFirstElement(index) {
+        return index === 0;
+      },
+      marginClass(index) {
+        return this.isFirstElement(index) ? 'margin-top' : 'extra-margin-top';
+      }
     }
   }
 </script>
