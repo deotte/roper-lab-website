b0720748fbb2c61e4502d3ecf64258dc38d756dd
Derrick Otte <derrickotte@gmail.com>
2022-01-19 16:58:15 -0500

    Add bootstrap to navbar
    - Looks good on Tablet, mobile and desktop

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,17 +1,33 @@
 <template>
-  <nav>
+  <nav class="navbar navbar-expand-lg navbar-light">
     <div class="container-fluid">
-      <div class="row">
-        <div class="col-4 v-center">
-          <img src="../assets/lab_logo.png">
-        </div>
-        <div class="col-8">
-          <router-link to="/">Home</router-link>
-          <router-link to="/research">Research</router-link>
-          <router-link to="/publications">Publications</router-link>
-          <router-link to="/people">People</router-link>
-          <router-link to="/contact">Contact</router-link>
-        </div>
+      <a class="navbar-brand" href="#">
+        <img src="@/assets/lab_logo.png" />
+      </a>
+      <button class="navbar-toggler"
+              type="button"
+              data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
+              aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+          <li class="nav-item mx-auto ms-lg-0">
+            <router-link to="/" class="nav-link">Home</router-link>
+          </li>
+          <li class="nav-item mx-auto ms-lg-4">
+            <router-link to="/publications" class="nav-link">Publications</router-link>
+          </li>
+          <li class="nav-item mx-auto ms-lg-4">
+            <router-link to="/research" class="nav-link">Research</router-link>
+          </li>
+          <li class="nav-item mx-auto ms-lg-4">
+            <router-link to="/people" class="nav-link">People</router-link>
+          </li>
+          <li class="nav-item mx-auto ms-lg-4">
+            <router-link to="/contact" class="nav-link">Contact</router-link>
+          </li>
+        </ul>
       </div>
     </div>
   </nav>
